c52c69b2e1507b6cd1a4a2ce1dcf455919c2dadc
erfansamandarian <esamandarian922@icloud.com>
2024-04-27 20:43:06 +1000

    minor 3

--- 3.cc
+++ 3.cc
@@ -9,13 +9,13 @@ void primeFactors(long int n) {
 
   for (long int i = 3; i <= sqrt(n); i = i + 2) {
     while (n % i == 0) {
-      std::cout << i << " ";
+      std::cout << i << ' ';
       n = n/i;
     }
   }
 
   if (n > 2)
-    std::cout << n << " ";
+    std::cout << n << ' ';
 }
 
 int main() {
